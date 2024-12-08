ba36d87a14c2032e2d7dbcd55046a6f18e65f736
mary1924 <masha.marunchak@gmail.com>
2024-12-08 23:25:56 +0200

    Add files via upload

--- lab_07/counter.js
+++ lab_07/counter.js
@@ -1,23 +1,46 @@
-const cards = document.querySelectorAll('.card');
-const numbers = [1, 1, 2, 2].sort(() => Math.random() - 0.5); 
+let score=0;
 let openCard = null;
+
+const start=()=>{
+    const cards = document.querySelectorAll('.card');
+const numbers = [1, 1, 2, 2].sort(() => Math.random() - 0.5); 
 
 cards.forEach((card, i) => {
     card.dataset.value = numbers[i]; 
     card.onclick = () => {
         if (card.textContent) return; 
         card.textContent = card.dataset.value; 
-
+console.log("card", card)
+console.log("openCard", openCard)
         if (!openCard) {
             openCard = card; 
         } else {
             if (openCard.dataset.value !== card.dataset.value) {
                 setTimeout(() => { 
-                    openCard.textContent = '';
+                
                     card.textContent = '';
                 }, 500);
+            } 
+            else {
+                score++;
+                document.getElementById('score').innerHTML=score;
+                openCard = null; 
             }
-            openCard = null; 
+        
         }
     };
 });
+}
+const reset=()=>{
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, i) => {
+        card.textContent = ''; 
+    } )
+}
+document.getElementById('startButton').addEventListener('click',start)
+document.getElementById('resetButton').addEventListener('click',()=>{
+    openCard = null; 
+    score=0;
+    document.getElementById('score').innerHTML=score;
+    reset();
+})
