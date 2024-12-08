fb1adbabc8287d6e04110063bfcc840eaf14dc48
mary1924 <masha.marunchak@gmail.com>
2024-12-08 22:40:32 +0200

    Add files via upload

--- lab_06/mouse.js
+++ lab_06/mouse.js
@@ -1,13 +1,11 @@
 const img = document.getElementById('myMouse'); 
-console.log(img)
 let x = window.innerWidth / 2; 
 let y = window.innerHeight / 2; 
-let speed = 5; 
 
 function updatePosition(event) {
 
-  x = Math.random() * window.innerWidth;
-  y = Math.random() * window.innerHeight;
+  x = Math.random() * window.innerWidth / 1.2;
+  y = Math.random() * window.innerHeight / 1.8;
 
   img.style.left = x + 'px';
   img.style.top = y + 'px';
