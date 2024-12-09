4eca683ee4040dddd31e93b46d7e1263bb9c2bac
mary1924 <masha.marunchak@gmail.com>
2024-12-10 01:43:07 +0200

    update

--- lab_12/pokeapi.js
+++ lab_12/pokeapi.js
@@ -2,40 +2,17 @@ document.addEventListener("DOMContentLoaded", fetchPokemonList);
 
 async function fetchPokemonList() {
     const url = `https://pokeapi.co/api/v2/pokemon?limit=20`; 
-
-    try {
         const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error('Не вдалося отримати список покемонів');
-        }
-
         const data = await response.json();
-
-       
         displayPokemonList(data.results);
-    } catch (error) {
-        console.error(error);
-        document.getElementById('pokemon-info').innerHTML = `<p class="error">Помилка: ${error.message}</p>`;
-    }
 }
+
 async function fetchPokemonData(url) {
-
-    try {
         const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error('Не вдалося отримати список покемонів');
-        }
-
         const data = await response.json();
-        console.log(data);
         document.getElementById('info').innerHTML = `<img src="${data.sprites.back_default}"/>` + `<img src="${data.sprites.front_default}"/>`; 
-        document.getElementById('info2').innerHTML = `<img src="${data.sprites.back_shiny}"/>` + `<img src="${data.sprites.front_shiny}"/>`  ; 
-    } catch (error) {
-        console.error(error);
-        document.getElementById('info').innerHTML = `<p class="error">Помилка: ${error.message}</p>`;
-    }
+        document.getElementById('info2').innerHTML = `<img src="${data.sprites.back_shiny}"/>` + `<img src="${data.sprites.front_shiny}"/>`  ;
+        document.getElementById('info3').innerHTML = '<div>Abilities: '+data.abilities.map(ability=> `<span> ${ability.ability.name}</span>` )+'</div>';  
 }
 
 
@@ -48,7 +25,7 @@ function displayPokemonList(pokemonList) {
 
     pokemonList.forEach(pokemon => {
         const li = document.createElement('li');
-        li.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        li.textContent = pokemon.name;
         ul.appendChild(li);
         li.addEventListener('click',()=>{
             fetchPokemonData(pokemon.url)
